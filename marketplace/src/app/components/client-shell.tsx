6663eab0a7d5efc126df07387fb53baa6ed94aea
Àlex Martínez <hey@alexmdevs.com>
2026-07-13 21:29:51 +0200

    reto 2 fix hydration error

--- marketplace/src/app/components/client-shell.tsx
+++ marketplace/src/app/components/client-shell.tsx
@@ -2,18 +2,14 @@
 
 "use client";
 
-import { useState } from "react";
-
 interface ClientShellProps {
   children: React.ReactNode;
 }
 
 export default function ClientShell({ children }: ClientShellProps) {
-  const [time] = useState(new Date().toLocaleTimeString());
-
   return (
     <div className="mt-4 px-4 py-2 bg-gray-600 text-white rounded">
-      <h3 className="font-bold mb-2">User Profile (Client Component) {time}</h3>
+      <h3 className="font-bold mb-2">User Profile (Client Component)</h3>
       {children}
     </div>
   );
